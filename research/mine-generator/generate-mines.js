4f132e25ab05c5151666862635b1d3c411a62a20
KonstantinSimeonov <kon.simeonov@protonmail.com>
2017-03-18 05:03:34 +0200

    Unit tested logic for mine distribution

--- research/mine-generator/generate-mines.js
+++ research/mine-generator/generate-mines.js
@@ -1,12 +1,35 @@
 'use strict';
 
+function countCells(hexagonSize) {
+    const n = hexagonSize,
+        result = (2 * (n - 1) * n) + (n - 2) * (n - 1) + 2 * n - 1;
+    return result;
+}
 
-function* getMineGenerator(hexagonSize, minesCount) {
-    if(minesCount >= hexagonSize) {
+function* getMineGenerator(options, hexagonSize, minesCount) {
+    let minesLeft = minesCount,
+        emptyCellsLeft = countCells(hexagonSize);
+    
+    if(minesCount >= emptyCellsLeft) {
         throw new Error(`Mines count(${minesCount}) cannot be more than or equal to the number of cells`);
     }
 
-    const mineProbabilityPerCell = minesCount / hexagonSize;
+    while(emptyCellsLeft >= 0) {
+        const randChance = Math.random(),
+            mineProbability = minesLeft / emptyCellsLeft;
+
+        if(mineProbability > randChance) {
+            yield options.getMine();
+            minesLeft -= 1;
+        } else {
+            yield options.getEmpty();
+        }
+
+        emptyCellsLeft -= 1;
+    }
 }
 
-console.log(countCells(3));
+module.exports = {
+    countCells,
+    getMineGenerator
+};
