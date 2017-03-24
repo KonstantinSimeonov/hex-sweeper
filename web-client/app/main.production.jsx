69b3a51e1d1cf3d0f9b5415393f676c5d77efd9e
KonstantinSimeonov <kon.simeonov@protonmail.com>
2017-03-24 17:12:59 +0200

    Fixed main.production.jsx

--- web-client/app/main.production.jsx
+++ web-client/app/main.production.jsx
@@ -6,5 +6,4 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 window.appConfig = Object.create({ serverDomain: 'http://hexsweepr.herokuapp.com' });
-console.log(window.appConfig);
-ReactDOM.render(<App appConfig={config} />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
